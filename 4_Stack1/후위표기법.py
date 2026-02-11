14a75f39c8f74aa38f7808f27bb5f68d491962d4
MINAH714 <jungmina1344@gmail.com>
2026-02-11 17:55:04 +0900

    순열

---
+++ 4_Stack1/후위표기법.py
@@ -0,0 +1,68 @@
+'''
+(6 + 5 *(2-8)/2)
+6528-*2/+
+'''
+stack = [0]*10
+top = -1
+
+icp = {'(':3, '*':2, '/':2, '+':1, '-':1}   # 스택 밖에서 우선순위
+isp = {'(':0, '*':2, '/':2, '+':1, '-':1}   # 스택 안에서 우선순위
+
+infix = '(6+5*(2-8)/2)'  # 중위식 문자열
+postfix = ''             # 후위식 문자열
+
+for token in infix:
+    # 피연산자면 후위식에 추가
+    if token not in '(*/+-)':
+        postfix += token
+    elif token == ')':    # 닫는 괄호면 여는 괄호를 만날때까지
+        while top>-1 and stack[top] != '(':
+            top -= 1
+            postfix += stack[top + 1]
+        # while stack and stack[-1] != '(':  #. append()/pop()
+        #     postfix += stack.pop()
+        if top != -1:
+            top -= 1      # '(' 제거
+    else:       # '(*/+-'사칙 연산자인 경우
+        if top == -1 or isp[stack[top]] < icp[token]:
+            top += 1
+            stack[top] = token
+        elif isp[stack[top]] >= icp[token]:
+            while top > -1 and isp[stack[top]] >= icp[token]:
+                top -= 1
+                postfix += stack[top + 1]
+            top += 1     # 스택의 마지막 연산자보다
+            stack[top] = token  # 우선순위가 높아졌으므로 push
+
+    print(postfix, stack, top)
+
+while top > -1:
+    top -= 1
+    postfix += stack[top + 1]
+
+print(postfix)
+
+stack = []
+for token in postfix:
+    if token not in '*/+-':      # 피연산자면 push
+        stack.append(int(token)) # token은 문자열
+    else:
+        op2 = stack.pop() # 오른쪽 피연산자
+        op1 = stack.pop() # 왼쪽 피연산자
+        result = 0
+        if token == '*':
+            # stack.append(op1*op2)
+            result = op1 * op2
+        elif token == '/':
+            result = op1 / op2
+        elif token == '-':
+            result = op1 - op2
+        elif token == '+':
+            result = op1 + op2
+        stack.append(result) # 연산결과 push
+        print(stack)
+
+answer = stack.pop()
+print(f'{answer:.0f}')
+print(f'{9.5:.0f}')
+print(f'{int(9.5)}')
